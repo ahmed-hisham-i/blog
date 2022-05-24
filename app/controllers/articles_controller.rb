61f965cea517eb3bf7c306ae6499a7771e5066ea
Ahmed Hisham <94743368+DO-DDY@users.noreply.github.com>
2022-05-24 10:22:27 +0200

    create articles from ui

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,26 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def index
+        @article = Article.all
+    end
+
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            flash[:notice] = "Article was sucessfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end 
     end
-    
+
+    def show 
+        @article = Article.find(params[:id])
+    end
+
     private
-    
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
