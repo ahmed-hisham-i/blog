e8f8cc2e3a797927ec9a8e83778e4bbdec0aa673
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-06 07:56:36 +0200

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220519113135) do
+ActiveRecord::Schema.define(version: 20220606054635) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20220519113135) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
