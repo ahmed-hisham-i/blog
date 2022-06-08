2c14896f8e56bea952d515e22da02604fc021926
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-08 04:41:44 +0200

    build sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
     
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "registeration successfully"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+
+
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
