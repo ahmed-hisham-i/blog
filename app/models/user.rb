d1109fc7fae1f97c3b4a224d5d3f0eca8a472241
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-08 03:36:03 +0200

    adding bcrypt to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
     format: { with: VALID_EMAIL_REGEX }
     before_save { self.email = email.downcase }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220606061155) do
+ActiveRecord::Schema.define(version: 20220608013047) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20220606061155) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
