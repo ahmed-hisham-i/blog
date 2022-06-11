eb464b31f3d3a1c4c4acafb928bef4d94f870c69
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-11 04:21:14 +0200

    add login/logout stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
   root to: 'pages#home'
+  get 'login', to: 'sessions#new'
   get 'signup', to: 'users#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
