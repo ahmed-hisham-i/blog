5e182486964498bb5991aa5f858abb6c1401cd3b
ahmedh <94743368+DO-DDY@users.noreply.github.com>
2022-06-11 05:10:00 +0200

    current login and fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was sucessfully created"
             redirect_to article_path(@article)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     
     def create 
         user = User.find_by(email: params[:session][:email].downcase)
-         if user  && user.authenticate(params[:session][:password])
+        if user  && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:success] = "You have logged in"
             redirect_to users_path(user)
@@ -21,5 +21,4 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
 
-    end
 end
